9b7efe8a6b3c8128d6f93acdcb6442a233d192b4
Daniyal-Imran09 <daniyalimran602@gmail.com>
2023-05-15 17:50:42 +0500

    Create about.js

---
+++ src/mycomponenets/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>
+      this an about page
+      <p>Lorem ipsum, dolor sit amet consectetur
+         adipisicing elit. Molestiae quasi impedit magnam architecto laboriosam officiis rem nihil voluptas magni! Aliquid,
+          eos blanditiis! Officiis architecto, delectus temporibus iusto beatae sunt iure.</p>
+    </div>
+  )
+}
+
+export default about
